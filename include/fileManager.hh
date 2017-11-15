475b9acfb8236ddb9c71c92aa771d2a6b918ea15
m.hiruxou <m.hiruxou@gmail.com>
2017-11-15 01:29:14 -0500

    push suite core + filemanager

---
+++ include/fileManager.hh
@@ -0,0 +1,15 @@
+#ifndef FILEMANAGER_HH_
+# define FILEMANAGER_HH_
+
+# include <iostream>
+# include <fstream>
+# include <vector>
+
+class fileManager
+{
+public:
+  fileManager();
+  ~fileManager();
+};
+
+#endif /*!FILEMANAGER_HH_*/
