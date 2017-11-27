2083bde0b99f5505985d11b81baf92dea292c431
Anaïs Foncel <anais.foncel@epitech.eu>
2017-11-27 01:29:56 +0100

    UPDATE Bombe.cpp

--- srcs/Bombe.cpp
+++ srcs/Bombe.cpp
@@ -5,34 +5,34 @@
 // Login   <foncel_a@epitech.net>
 // 
 // Started on  Sun Nov 12 11:02:03 2017 Anaïs Foncel
-// Last update Sun Nov 26 18:09:34 2017 Anaïs Foncel
+// Last update Mon Nov 27 01:08:53 2017 Anaïs Foncel
 //
 
 #include "Bombe.hh"
 
-Bomber::Bombe::Bombe(Bomber::v2d const &pos, int range)
+bomber::Bombe::Bombe(bomber::v2d const &pos, int range)
   : _x(pos.X), _y(pos.Y), _range(range)
 {
 
 }
 
-Bomber::Bombe::~Bombe() {}
+bomber::Bombe::~Bombe() {}
 
 
 /*
 ** Getter
 */
-int		Bomber::Bombe::getX() const { return this->_x; }
+int		bomber::Bombe::getX() const { return this->_x; }
 
-int		Bomber::Bombe::getY() const { return this->_y; }
+int		bomber::Bombe::getY() const { return this->_y; }
 
-int		Bomber::Bombe::getRange() const { return this->_range; }
+int		bomber::Bombe::getRange() const { return this->_range; }
 
-Bomber::v2d	Bomber::Bombe::getPos() const { return Bomber::v2d(_x, _y); }
+bomber::v2d	bomber::Bombe::getPos() const { return bomber::v2d(_x, _y); }
 
-irr::u32	Bomber::Bombe::getTime() const { return this->_time; }
+irr::u32	bomber::Bombe::getTime() const { return this->_time; }
 
 /*
 ** Setter
 */
-void		Bomber::Bombe::setTime(irr::u32 time) { this->_time = time; }
+void		bomber::Bombe::setTime(irr::u32 time) { this->_time = time; }
